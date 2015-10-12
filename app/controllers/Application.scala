650cfe48e1d4381c9940dd76302501fb3fe402fa
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2015-10-12 17:54:40 +0900

    画像一覧を取得してくる部分作成

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,10 +2,12 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models.PictureModel
 
 class Application extends Controller {
 
   def index = Action {
+    val future = PictureModel.pictures()
     Ok(views.html.index("Your new application is ready."))
   }
 
--- app/models/PictureModel.scala
+++ app/models/PictureModel.scala
@@ -1,6 +1,6 @@
 package models
 
-import repositories._
+import repositories.Tables.{Picture, PictureRow}
 import slick.driver.PostgresDriver.api._
 import scala.concurrent.Future
 import entities.OriginalPicture
@@ -18,16 +18,42 @@ object PictureModel extends PictureModelTrait {
 trait PictureModelTrait {
   val db = Database.forConfig("pg_database")
 
+  /**
+   * データベースにストアする
+   * 
+   * @param originalPicture: OriginalPicture
+   * 
+   * @return Future[Option[Long]] id
+   */
   def insert(originalPicture: OriginalPicture): Future[Option[Long]] = {
-    val pictureRow = Tables.PictureRow(
+    val pictureRow = PictureRow(
       0,
       originalPicture.fileName,
       originalPicture.contentType,
       originalPicture.uploadedAt)
     val action = for {
-      id <- Tables.Picture returning Tables.Picture.map(_.id) += pictureRow
+      id <- Picture returning Picture.map(_.id) += pictureRow
     } yield Some(id)
     db.run(action)
+  }
+
+  /**
+   * データベースから最新画像を指定した数だけ取得してくる
+   * 
+   * @param count: Int 取得してくる数。デフォルトでは20
+   * 
+   * @return Future[Option[Seq[PictureRow]]] エラーの場合None
+   */
+  def pictures(
+    count: Int = 20): Future[Option[Seq[PictureRow]]] = {
+    val query = Picture.sortBy(_.createdAt.desc).take(count)
+    val action = query.result
+    db.run(action).map {
+      case pictures: Seq[PictureRow]  => Some(pictures)
+      case _ => None
+    }.recover {
+      case e => None // TODO: ログ吐いたりするようにする
+    }
   }
 
 }
