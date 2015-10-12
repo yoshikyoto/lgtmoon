7a8c8811241d24e1da71a0957b342bb7b2f4168e
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2015-10-12 18:58:45 +0900

    トップページに画像一覧を表示した

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,14 +1,30 @@
 package controllers
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 import play.api._
 import play.api.mvc._
 import models.PictureModel
+import utils.UrlBuilder
 
+/**
+ * indexページを表示するコントローラー
+ */
 class Application extends Controller {
 
-  def index = Action {
-    val future = PictureModel.pictures()
-    Ok(views.html.index("Your new application is ready."))
+  /**
+   * indexページを表示する
+   */
+  def index = Action.async {
+    PictureModel.pictures().map {
+      case Some(pictures) => {
+        val pictureUrls = pictures.map {
+          picture => UrlBuilder.imageUrl(picture.fileName)
+        }
+        Ok(views.html.index(pictureUrls))
+      }
+      case None => InternalServerError("")
+    }
   }
 
 }
