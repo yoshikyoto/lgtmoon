e3e173fc514a6644d4fcd9f4db8a9e897b238da5
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2016-01-31 08:43:55 +0900

    POST keyword.jsonの削除

--- app/controllers/ImageController.scala
+++ app/controllers/ImageController.scala
@@ -18,42 +18,12 @@ import actors.ImageActor
 import actors.ImageGenerateMessage
 import utils._
 
-/**
- * imagesのpost,getを行うためのcontroller
- */
+/** LGTMoonが持っているimagesの情報を返すcontroller */
 class ImageController extends BaseControllerTrait {
-  val imageActor = Akka.system.actorOf(Props(new ImageActor()))
-
-  def postKeyword = Action.async { request =>
-    val jsonOpt = request.body.asJson
-    jsonOpt match {
-      case Some(json) => {
-        val keywordOpt = (json \ "keyword").asOpt[String]
-        keywordOpt match {
-          case Some(keyword) => {
-            // とりあえずURLだけ先に払い出して返す
-            ImageModel.create() map {
-              case Some(id) => {
-                val url = UrlBuilder.imageUrl(id.toString)
-                // 動画のダウンロードと変換を要請
-                imageActor ! ImageGenerateMessage(id, keyword)
-                Ok(JsonBuilder.imageUrl(url))
-              }
-              case None => DATABASE_CONNECTION_ERROR_RESPONSE
-            }
-          }
-          case None => Future(PARAMETER_KEYWORD_NOT_FOUND_RESPONSE)
-        }
-      }
-      case None => Future(NOT_JSON_RESPONSE)
-    }
-  }
-
   def recent = Action.async { request =>
     ImageModel.images(20).map {
-      case Some(images) => Ok(JsonBuilder.images(images))
       case _ => InternalServerError("")
+      case Some(images) => Ok(JsonBuilder.images(images))
     }
   }
-
 }
