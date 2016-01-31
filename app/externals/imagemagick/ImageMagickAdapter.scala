d8fa1dc62e9c2763c91179f0593b85cf14c148b8
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2016-01-31 13:36:24 +0900

    imagemagickで仕様するフォントなどの変更

--- app/externals/imagemagick/ImageMagickAdapter.scala
+++ app/externals/imagemagick/ImageMagickAdapter.scala
@@ -4,37 +4,51 @@ import java.io._
 import play.api.Play
 import org.im4java.core._
 
-/**
- * imagemagickを利用するためのAdapter
- */
+/** imagemagickを利用するためのAdapter */
 class ImageMagickAdapter() {
+  /** imagemagickのバイナリのディレクトリ */
   val imagemagickDir = Play.current.configuration.getString("imagemagick.dir").get
+  /** フォントファイルがあるディレクトリ */
   val fontDir = Play.current.configuration.getString("imagemagick.fontDir").get
 
   /**
    * 画像の変換を行う
+   *
+   * @param beforePath: String 生成前の画像のPATH
+   * @param afterPath: String 生成後の画像のPATH
    */
   def convert(
     beforePath: String,
     afterPath: String) {
     val operation = new IMOperation()
     operation.addImage(beforePath)
-    operation.geometry(320, 320)
+    operation.geometry(480, 480)
+    // LGTMの文字
     operation.gravity("center")
-    operation.font(font("AvenirNext.ttc"))
-    operation.pointsize(60)
-    println((font("AvenirNext.ttc")))
+    operation.font(font("Aileron-Black.otf"))
+    operation.pointsize(96)
     operation.stroke("none")
     operation.fill("white")
+    operation.kerning(16);
+    operation.strokewidth(5);
     operation.annotate(0, 0, 0, 0, "LGTM")
+    // Looks Good To Me の文字
+    operation.font(font("Aileron-Light.otf"))
+    operation.pointsize(12)
+    operation.kerning(10);
+    operation.annotate(0, 0, 0, 64, "Looks  Good  To  Me")
     operation.addImage(afterPath)
+    // コマンド実行
     val command = new ConvertCmd()
     command.setSearchPath(imagemagickDir)
     command.run(operation)
   }
 
   /**
-   * フォントの絶対パスを取得する
+   * imagemagickに渡すべきフォントのパスを取得する
+   *
+   * @param fontName: String e.g. "Aileron-Regular.otf"
+   * @return String ImageMagickに渡すべきフォントのパス
    */
   def font(fontName: String): String = {
     fontDir + "/" + fontName
