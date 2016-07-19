e3f61228fd40c200e27be0031b6d19a9c459d800
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2016-07-19 17:40:30 +0900

    APIのレスポンスを追加

--- app/controllers/ImageSearchController.scala
+++ app/controllers/ImageSearchController.scala
@@ -1,27 +1,25 @@
 package controllers
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import play.api._
 import play.api.mvc._
-import org.joda.time.DateTime
-import play.api.libs.Files.TemporaryFile
-import play.api.mvc.MultipartFormData.FilePart
 import play.api.libs.json._
-import play.api.Play.current
-import play.api.libs.concurrent.Akka
-import akka.actor.Props
-import externals.google.CustomSearchAdapter
-import utils._
+import play.api.libs.functional.syntax._
+import scala.concurrent.ExecutionContext.Implicits.global
+import domain.image.search.ImageSearchService
+import domain.image.search._
 
 /** 画像検索コントローラー */
 class ImageSearchController extends BaseControllerTrait {
+  implicit val imageWrites = (
+    (__ \ "url").write[String] and
+      (__ \ "mime").write[String]
+  )(unlift(Image.unapply))
+
   /** キーワードを受け取り検索結果を返す */
   def search(keyword: String) = Action.async { request =>
-    CustomSearchAdapter.imageUrls(keyword).map {
+    ImageSearchService.images(keyword).map {
       case None => CUSTOM_SEARCH_ERROR_RESPONSE
-      case Some(urls) => {
-        Ok(JsonBuilder.imageUrls(urls))
+      case Some(images) => {
+        Ok(Json.obj("images" -> Json.toJson(images)))
       }
     }
   }
