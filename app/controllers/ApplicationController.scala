94a5dca284f6c979f0adf84bf52726e9d67b1586
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-11-12 19:16:53 +0900

    add log

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -7,6 +7,9 @@ import play.api.mvc._
 /** トップページを表示するためのController */
 class ApplicationController extends Controller {
   def index = Action { request =>
+    Logger.info("remoteAddress: " + request.remoteAddress +
+      "\tx-forwarded-for: " + request.headers.get("x-forwarded-for").getOrElse("") +
+      "\tx-real-ip: " + request.headers.get("x-real-ip").getOrElse(""))
     Ok(views.html.index())
   }
 }
