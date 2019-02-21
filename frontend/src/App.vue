1a531f16c5bf82d1bfc56838a3404e79ed1eb0f4
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-02-21 16:23:27 +0900

    fix help

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,65 +17,68 @@
     </div>
     <Recent v-show="selected == 0"/>
     <Random v-show="selected == 1"/>
+    <Help v-show="selected == 2"/>
   </div>
 </template>
 
 <script>
- import Bar from '@/components/Bar'
- import Recent from '@/components/Recent'
- import Random from '@/components/Random'
+  import Bar from '@/components/Bar'
+  import Recent from '@/components/Recent'
+  import Random from '@/components/Random'
+  import Help from '@/components/Help'
 
- export default {
-     name: 'app',
-     data() {
-         return {
-             selected: 0
-         }
-     },
-     components: {
-         Bar,
-         Recent,
-         Random
-     },
-     methods: {
-         recent() {
-             this.selected = 0
-         },
-         random() {
-             this.selected = 1
-         },
-         help() {
-             this.selected = 2
-         }
-     }
- }
+  export default {
+    name: 'app',
+    data() {
+      return {
+        selected: 0
+      }
+    },
+    components: {
+      Bar,
+      Recent,
+      Random,
+      Help
+    },
+    methods: {
+      recent() {
+        this.selected = 0
+      },
+      random() {
+        this.selected = 1
+      },
+      help() {
+        this.selected = 2
+      }
+    }
+  }
 </script>
 
 <style>
- .center {
+  .center {
     margin-left: auto;
     margin-right: auto;
- }
- .menu:after {
-     clear: both;
- }
- .menu-item {
-     width: 300px;
-     padding: 5px 0px;
-     text-align: center;
-     vertical-align: middle;
-     float: left;
-     cursor: pointer;
-     font-size: 1.5em;
-     font-weight: bold;
-     color: #300;
- }
- .menu-item:hover {
-     background: #ccf;
-     color: #600;
- }
- .menu-item-selected {
-     background: #009;
-     color: #ffffff;
- }
+  }
+  .menu:after {
+    clear: both;
+  }
+  .menu-item {
+    width: 300px;
+    padding: 5px 0px;
+    text-align: center;
+    vertical-align: middle;
+    float: left;
+    cursor: pointer;
+    font-size: 1.5em;
+    font-weight: bold;
+    color: #300;
+  }
+  .menu-item:hover {
+    background: #ccf;
+    color: #600;
+  }
+  .menu-item-selected {
+    background: #009;
+    color: #ffffff;
+  }
 </style>
