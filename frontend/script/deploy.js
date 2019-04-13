fd3d4d3b87c6d2c8da869830a59bf43526965692
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-04-13 09:23:46 +0900

    deploy twitter button

--- frontend/script/deploy.js
+++ frontend/script/deploy.js
@@ -4,14 +4,7 @@ const src = './dist'
 const dest = '../public'
 const template = '../app/views'
 
-// html
-const htmls = ['index']
-for (const html of htmls) {
-  cpx.copySync(`${src}/${html}.html`, `${template}/${html}.scala.html`)
-
-  console.log(html)
-}
-
 // css, script
 cpx.copySync(`${src}/assets/js/*.{js,js.map}`, `${dest}/js/`)
 cpx.copySync(`${src}/assets/css/*.css`, `${dest}/css/`)
+cpx.copySync(`${src}/*.html`, `${template}/`)
