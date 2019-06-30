11c561a49a2f2f03f18d478da3d7743ddb7b0dcd
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-06-30 20:51:20 +0900

    ImplementedBy

--- app/images/ImageSearcher.scala
+++ app/images/ImageSearcher.scala
@@ -2,9 +2,14 @@ package image
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import com.google.inject.ImplementedBy
 
+@ImplementedBy(classOf[GoogleCustomSearchClient])
+trait ImageSearcher {
+  def urls(keyword: String): Future[Option[Seq[String]]]
+}
 
-class ImageSearcher {
+class GoogleCustomSearchClient extends ImageSearcher{
   def urls(keyword: String): Future[Option[Seq[String]]] = {
     return Future(Some(Seq("http://test.example")))
   }
