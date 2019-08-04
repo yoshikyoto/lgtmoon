49ce30add68cd61234756ba5ec375cbc9d7d408b
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-08-04 16:50:04 +0900

    ImageBinaryControllerがBaseController脱脚

--- app/controllers/ImageBinaryController.scala
+++ app/controllers/ImageBinaryController.scala
@@ -1,9 +1,13 @@
 package controllers
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import play.mvc.Controller
-import play.api.mvc.Results.NotFound
-import play.api.mvc.{Action, Result, ResponseHeader}
+import play.api.mvc.{
+  Action,
+  AnyContent,
+  Controller,
+  ResponseHeader,
+  Result
+}
 import play.api.http.HttpEntity
 import image.ImageRepository
 import akka.util.ByteString
@@ -14,13 +18,15 @@ class ImageBinaryController @Inject() (
   val imageRepository: ImageRepository
 ) extends Controller {
   /** idを受け取り画像のバイナリデータを返す */
-  def image(id: Int)  = Action.async { request =>
+  def image(id: Int): Action[AnyContent]  = Action.async { request =>
     imageRepository.binary(id).map {
       case Some(bin) => {
-        /** bin: Array[Byte] */
         Result(
           header = ResponseHeader(200),
-          body =  HttpEntity.Strict(ByteString.fromArray(bin), Some("image/png"))
+          body =  HttpEntity.Strict(
+            ByteString.fromArray(bin),
+            Some("image/png")
+          )
         ).withHeaders(
           // 1週間
           "Cache-Control" -> "max-age=604800"
