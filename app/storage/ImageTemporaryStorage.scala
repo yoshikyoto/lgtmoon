eaa4f85adafa1dcd7fa7b65422a30d92596582d9
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-08-04 23:23:17 +0900

    implements ImageTemporaryStorage

---
+++ app/storage/ImageTemporaryStorage.scala
@@ -0,0 +1,29 @@
+package storage
+
+import com.google.inject.Inject
+import java.net.URL
+import play.api.Configuration
+import java.io.{
+  BufferedOutputStream,
+  FileOutputStream
+}
+
+/** 画像のバイナリを一時的に保存するストレージ */
+class ImageTemporaryStorage @Inject() (
+  config: Configuration
+){
+  /** 変換前の画像が保存されるパス */
+  val srcImageDir = config.get[String]("storage.image.src.dir")
+
+  /** URLから画像をダウンロードしてきて
+   * 変換前の画像ストレージに保存する */
+  def save(id: Int, url: String): Unit = {
+    val srcStream = new URL(url).openStream
+    val srcBuffer = Stream.continually(srcStream.read).takeWhile(-1 !=).map(_.byteValue).toArray
+    val destPath = srcImageDir + "/" + id
+    val destStream = new BufferedOutputStream(new FileOutputStream(destPath))
+    destStream.write(srcBuffer)
+    destStream.close()
+    srcStream.close()
+  }
+}
