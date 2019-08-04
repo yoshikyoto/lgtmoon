254378ac13319d30297a7ecc8c7775ffcc432276
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-08-04 23:40:30 +0900

    can get convert destination file path

--- app/storage/ImageTemporaryStorage.scala
+++ app/storage/ImageTemporaryStorage.scala
@@ -13,17 +13,28 @@ class ImageTemporaryStorage @Inject() (
   config: Configuration
 ){
   /** 変換前の画像が保存されるパス */
-  val srcImageDir: String = config.get[String]("storage.image.src.dir")
+  val srcDir: String = config.get[String]("storage.image.src.dir")
+
+  /** 変換後の画像が保存されるパス */
+  val destDir: String = config.get[String]("storage.image.dest.dir")
+
+  def srcPath(id: Int): String = srcDir + "/" + id
+
+  def destPath(id: Int): String = destDir + "/" + id
 
   /** URLから画像をダウンロードしてきて
-   * 変換前の画像ストレージに保存する */
-  def save(id: Int, url: String): Unit = {
-    val srcStream = new URL(url).openStream
-    val srcBuffer = Stream.continually(srcStream.read).takeWhile(-1 !=).map(_.byteValue).toArray
-    val destPath = srcImageDir + "/" + id
-    val destStream = new BufferedOutputStream(new FileOutputStream(destPath))
-    destStream.write(srcBuffer)
-    destStream.close()
-    srcStream.close()
+   * 変換前の画像ストレージに保存する
+   * @return 保存されたバイナリの絶対パス */
+  def save(id: Int, url: String): String = {
+    val downloadSrcStream = new URL(url).openStream
+    val downloadSrcBuffer = Stream.continually(downloadSrcStream.read).takeWhile(-1 !=).map(_.byteValue).toArray
+    // ダウンロードされたファイルはLGTM画像変換のためのソース画像のPATHに入る
+    val downloadDestPath = srcPath(id)
+    val downloadDestStream = new BufferedOutputStream(new FileOutputStream(downloadDestPath))
+    downloadDestStream.write(downloadSrcBuffer)
+    downloadDestStream.close()
+    downloadSrcStream.close()
+    return downloadDestPath
   }
+
 }
