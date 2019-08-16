92a48d7390738ec233ff28d9a99b639e3872310b
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-08-16 11:40:23 +0900

    starできるように

--- frontend/src/components/LgtmImage.vue
+++ frontend/src/components/LgtmImage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="image-block">
-    <img :src="starIcon" class="star">
+  <div class="image-block" @mouseover="mouseover()" @mouseout="mouseout()">
+    <img v-if="isShowStar" :src="starIcon" class="star" @mousedown="toggleStar()">
     <img :src="item.url" v-on:click="select()" class="image">
   </div>
 </template>
@@ -8,17 +8,47 @@
 <script>
   export default {
     name: "LgtmImage",
+    data() {
+      return {
+        isMouseover: false,
+        isStared: false,
+      }
+    },
     props: [
       'item'
     ],
     computed: {
       starIcon() {
+        if (this.isStared) {
+          return '/assets/star-on.png'
+        }
         return '/assets/star-off.png'
+      },
+      isShowStar() {
+        // すでにスターしている場合はスターを表示してやる
+        if (this.isStared) {
+          return true
+        }
+        // そうでない場合はマウスオーバー次第
+        return this.isMouseover
       }
     },
     methods: {
       select() {
         this.$emit('select', this.item)
+      },
+      mouseover() {
+        this.isMouseover = true
+      },
+      mouseout() {
+        this.isMouseover = false
+      },
+      toggleStar() {
+        if (this.isStared) {
+          this.isStared = false
+        } else {
+          this.isStared = true
+        }
       }
     }
   }
