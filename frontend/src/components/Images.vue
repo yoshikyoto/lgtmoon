361944cee9156eb3b896a60c377e010f45ecbc5c
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-08-16 11:32:10 +0900

    一つのImageのcomponentを分ける

--- frontend/src/components/Images.vue
+++ frontend/src/components/Images.vue
@@ -1,45 +1,46 @@
 <template>
   <div class="images-block">
+    <!--
     <div v-for="item in items" class="image-block">
+      <img :src="starIcon" class="star">
       <img :src="item.url" v-on:click="select(item)" class="image">
     </div>
+    -->
+    <LgtmImage v-for="item in items" :item="item" @select="select">
+    </LgtmImage>
   </div>
 </template>
 
 <script>
- export default {
-     name: 'Images',
-     props: [
-         'items'
-     ],
-     methods: {
-         select(item) {
-             this.$emit('select', item)
-         }
-     }
- }
+  import LgtmImage from '@/components/LgtmImage'
+
+  export default {
+    name: 'Images',
+    props: [
+      'items'
+    ],
+    components: {
+      LgtmImage
+    },
+    methods: {
+      select(item) {
+        this.$emit('select', item)
+      }
+    }
+  }
 </script>
 
-<style>
- .images-block {
-     width: 100%;
-     -moz-column-count: 4;
-     -webkit-column-count: 4;
-     -o-column-count: 4;
-     -ms-column-count: 4;
-     -moz-column-gap: 4px;
-     -webkit-column-gap: 4px;
-     -o-column-gap: 4px;
-     -ms-column-gap: 4px;
-     box-sizing: border-box;
- }
- .image-block {
-    display: inline-block;
-    box-sizing: border-box;
-    border: 1px solid #ccc;
- }
- .image {
+<style scoped>
+  .images-block {
     width: 100%;
-    height: auto;
- }
+    -moz-column-count: 4;
+    -webkit-column-count: 4;
+    -o-column-count: 4;
+    -ms-column-count: 4;
+    -moz-column-gap: 4px;
+    -webkit-column-gap: 4px;
+    -o-column-gap: 4px;
+    -ms-column-gap: 4px;
+    box-sizing: border-box;
+  }
 </style>
---
+++ frontend/src/components/LgtmImage.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="image-block">
+    <img :src="starIcon" class="star">
+    <img :src="item.url" v-on:click="select()" class="image">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "LgtmImage",
+    props: [
+      'item'
+    ],
+    computed: {
+      starIcon() {
+        return '/assets/star-off.png'
+      }
+    },
+    methods: {
+      select() {
+        this.$emit('select', this.item)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .image-block {
+    position: relative;
+    display: inline-block;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+  }
+  .image {
+    width: 100%;
+    height: auto;
+  }
+  .star {
+    height: 30px;
+    width: 30px;
+    position: absolute;
+    top: 2px;
+    right: 2px;
+  }
+</style>
