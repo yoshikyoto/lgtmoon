af430799ab90231a7e7001db489bd94b3adc7c7f
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-08-16 10:47:52 +0900

    update deploy shell

--- frontend/script/deploy.js
+++ frontend/script/deploy.js
@@ -3,7 +3,14 @@ const cpx = require('cpx')
 const src = './dist'
 const dest = '../public'
 
-// css, script
+console.log("Copy JS")
 cpx.copySync(`${src}/assets/js/*.{js,js.map}`, `${dest}/js/`)
+
+console.log("Copy CSS")
 cpx.copySync(`${src}/assets/css/*.css`, `${dest}/css/`)
+
+console.log("Copy HTML")
 cpx.copySync(`${src}/*.html`, `${dest}/`)
+
+console.log("Copy assets")
+cpx.copySync(`${src}/assets/fonts/*`, `${dest}/fonts/`)
