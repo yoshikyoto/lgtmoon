c03c8eac2b6ff28ac223f118a566b117293a39aa
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-08-23 01:18:24 +0900

    ヘルプこうしn

--- frontend/src/components/Help.vue
+++ frontend/src/components/Help.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <div class="lgtmoon-section center">
+    <section class="lgtmoon-section center">
+      <h2 class="help-section-title">LGTMoonとは</h2>
+      LGTMoonはLGTM画像をカンタンに生成・作成できるWebサイト、「LGTM画像ジェネレーター」です。
+      GIFアニメーションからもLGTM画像を生成できます。
+    </section>
+
+    <section class="lgtmoon-section center">
       <h2 class="help-section-title">使い方</h2>
 
       キーワード検索を使ってLGTM画像を生成する方法と、画像URLからLGTM画像を生成する方法があります。
@@ -15,27 +21,9 @@
       <ol>
         <li>httpまたはhttpsから始まる画像URLを検索欄に入力すると、その画像をもとにLGTM画像が生成されます。</li>
       </ol>
-    </div>
+    </section>
 
-    <FavoriteHelp />
-
-    <div class="lgtmoon-section center">
-      <h2 class="help-section-title">LGTMとは</h2>
-      GitHubなどでプルリク（プルリクエスト/Pull Request）をレビューした時、「Looks Good To Me」
-      （自分がレビューした限りはよいと思います、マージしていいですよ、くらいの意味）
-      という言葉を略して「LGTM」と書くことがあります。
-      このLGTMをちょっと楽しい画像にすることで、レビューすることやプルリクを出すことがちょっと楽しくなります。<br>
-      <img src="https://qiita-image-store.s3.amazonaws.com/0/29024/373aad85-d1aa-96d7-026c-66b3587ae536.png" class="center">
-    </div>
-
-    <div class="lgtmoon-section center">
-      <h2 class="help-section-title">LGTMoonとは</h2>
-      LGTMoonはLGTM画像をカンタンに生成・作成できるWebサイト、「LGTM画像ジェネレーター」です。
-      なるべくシンプルに、カンタンに、手間を掛けずLGTM画像を作ることのできるアプリです。
-      静止画だけでなく、GIFアニメーションなんかからもLGTM画像を生成できます。
-    </div>
-
-    <div class="lgtmoon-section center">
+    <section class="lgtmoon-section center">
       <h2 class="help-section-title">注意</h2>
 
       <h3>画像生成時の注意</h3>
@@ -44,6 +32,7 @@
         <li>生成に成功すると最近の画像一覧に画像が表示されます。</li>
         <li>画像生成に失敗する可能性もあります。しばらくたっても生成されない場合は、もう一度お試しいただくか、別の画像でお試しください。</li>
         <li>LGTM画像生成の際は、元画像の著作権などに注意してください。</li>
+        <li>公序良俗に反する画像や違法な画像を作成しないでください。これらの画像が見つかった場合は予告なく画像を削除することがあります。</li>
       </ul>
 
       <h3 class="help-section-title">画像検索のヒント</h3>
@@ -60,13 +49,23 @@
         <li>
           <a href="https://www.google.com/settings/ads" target="_blank">広告設定</a>
           でパーソナライズ広告を無効にしたり、
-          <a href="http://www.aboutads.info/" target="_blank">www.aboutads.info</a>
+          <a href="https://www.aboutads.info/" target="_blank">www.aboutads.info</a>
           で、パーソナライズ広告に使われる第三者配信事業者の Cookie を無効にすることができます。
         </li>
       </ul>
-    </div>
+    </section>
+    <FavoriteHelp />
 
-    <div class="lgtmoon-section center">
+    <section class="lgtmoon-section center">
+      <h2 class="help-section-title">LGTMとは</h2>
+      GitHubなどでプルリク（プルリクエスト/Pull Request）をレビューした時、「Looks Good To Me」
+      （自分がレビューした限りはよいと思います、マージしていいですよ、くらいの意味）
+      という言葉を略して「LGTM」と書くことがあります。
+      このLGTMをちょっと楽しい画像にすることで、レビューすることやプルリクを出すことがちょっと楽しくなります。<br>
+      <img src="https://qiita-image-store.s3.amazonaws.com/0/29024/373aad85-d1aa-96d7-026c-66b3587ae536.png" class="center">
+    </section>
+
+    <section class="lgtmoon-section center">
       <h2 class="help-section-title">Contact</h2>
       何か要望・ご連絡がございましたら下記twitterなどにご連絡ください。
       GitHubにプルリクエストを出して頂いても構いません。
@@ -102,9 +101,9 @@
           </ul>
         </li>
       </ul>
-    </div>
+    </section>
 
-    <div class="lgtmoon-section center">
+    <section class="lgtmoon-section center">
       <h2 class="help-section-title">LGTMoonは無料で利用できます</h2>
       <div>
         <ul>
@@ -115,7 +114,7 @@
           </li>
         </ul>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
