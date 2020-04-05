f2457dde360ba1963f3c13ffa811fb4ce0fd7ca5
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2020-04-05 10:00:50 +0900

    多言語対応

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from '@/App.vue'
 import VueI18n from 'vue-i18n'
+import messages from './message'
 
 Vue.use(VueI18n);
 
-const messages = {};
 const i18n = new VueI18n({
-  locale: 'ja',
+  // en-US, ja-JP などの en, jp の部分を取得
+  locale: navigator.language.split('-')[0],
+  // 対応する言語が無い場合は英語にする
+  fallbackLocale: 'en',
   messages,
 })
 
