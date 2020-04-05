f1706cbc7d84dfe7ebc805bc8cf631a7816b51ef
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2020-04-05 09:44:14 +0900

    i18nをvueに追加

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from '@/App.vue'
+import VueI18n from 'vue-i18n'
+
+Vue.use(VueI18n);
+
+const messages = {};
+const i18n = new VueI18n({
+  locale: 'ja',
+  messages,
+})
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  i18n
+}).$mount('#app');
