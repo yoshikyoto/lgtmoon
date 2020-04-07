eabd325b95df9c1a6598aa6b3cf75a5a14f49e44
yoshiyuki-sakamoto <stb_yoshiyuki-s@hotmail.co.jp>
2020-04-07 23:25:37 +0900

    言語切替ボタン

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,17 +2,15 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import VueI18n from 'vue-i18n'
 import messages from './message'
-import locale from './modules/Locale'
 
 Vue.use(VueI18n);
 
+console.log(locale);
 const i18n = new VueI18n({
-  // en-US, ja-JP などの en, jp の部分を取得
-  locale: locale.getLocale(),
-  // 対応する言語が無い場合は英語にする
-  fallbackLocale: 'en',
-  messages,
-})
+  // locale は index.html や en.html に埋め込まれている
+  locale: locale,
+  messages
+});
 
 new Vue({
   render: h => h(App),
