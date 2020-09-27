ad49318a99e0d485978a51add3600db9ac0cf492
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2020-09-27 10:00:08 +0900

    warningが出ていたので修正

--- app/image/ImageConverter.scala
+++ app/image/ImageConverter.scala
@@ -4,5 +4,5 @@ import com.google.inject.ImplementedBy
 
 @ImplementedBy(classOf[command.ImageMagick])
 trait ImageConverter {
-  def convert(srcPath: String, destPath: String)
+  def convert(srcPath: String, destPath: String): Unit
 }
