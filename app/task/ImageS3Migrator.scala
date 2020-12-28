ae4c58d0be0cc93b8c563b050c5bf71a1393fa8d
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2020-12-29 00:27:35 +0900

    S3への移行タスクが動くようにする

--- app/task/ImageS3Migrator.scala
+++ app/task/ImageS3Migrator.scala
@@ -7,6 +7,16 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import com.google.inject.Inject
 import image.{ImageRepository, ImageStorage, ImageTemporaryStorage}
 
+/**
+ * Heroku PostgreSQL の容量の問題のため、
+ * 画像を PostgreSQL から S3 に移行するための処理
+ *
+ * application.conf に
+ * play.modules.enabled += "task.TaskModule"
+ * の設定を追加すると動く。
+ *
+ * @see https://www.playframework.com/documentation/2.8.x/ScheduledTasks
+ */
 class ImageS3Migrator @Inject() (
   val imageRepository: ImageRepository,
   val imageTemporaryStorage: ImageTemporaryStorage,
