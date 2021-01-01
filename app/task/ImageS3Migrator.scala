64165f11f6c5ecc1ff35c192d14fe088ed7b3b92
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2021-01-01 22:41:43 +0900

    レンジを修正

--- app/task/ImageS3Migrator.scala
+++ app/task/ImageS3Migrator.scala
@@ -22,7 +22,7 @@ class ImageS3Migrator @Inject() (
   val imageTemporaryStorage: ImageTemporaryStorage,
   val imageStorage: ImageStorage
 ) {
-  val startId = 15312
+  val startId = 18764
   val endId = 112000
   // val endId = 112000
   Future {
