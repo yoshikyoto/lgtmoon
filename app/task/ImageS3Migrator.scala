0ff6d694caf3ca8d43bbd46681dc8f8e430b3f31
Yoshiyuki Sakamoto <yoshikyoto@users.noreply.github.com>
2021-01-15 14:37:13 +0900

    Update ImageS3Migrator.scala

--- app/task/ImageS3Migrator.scala
+++ app/task/ImageS3Migrator.scala
@@ -22,7 +22,7 @@ class ImageS3Migrator @Inject() (
   val imageTemporaryStorage: ImageTemporaryStorage,
   val imageStorage: ImageStorage
 ) {
-  val startId = 101808
+  val startId = 106732
   val endId = 112000
   // val endId = 112000
   Future {
