479d689d4919b3da9d867abc629574b9c1d379a0
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2021-05-01 12:40:01 +0900

    これでもとりあえず動いた

--- app/payment/paypal/Client.scala
+++ app/payment/paypal/Client.scala
@@ -1,7 +1,6 @@
 package payment.paypal
 
 import java.util.Base64
-
 import scala.concurrent.ExecutionContext.Implicits.global
 import com.google.inject.Inject
 import play.api.Configuration
@@ -13,11 +12,12 @@ import scala.concurrent.Future
 
 class Client @Inject() (
   config: Configuration,
-  ws: WSClient
+  ws: WSClient,
 ) {
   private val apiBaseUrl = config.get[String]("paypal.apiBaseUrl")
   private val clientId = config.get[String]("paypal.clientId")
   private val secret = config.get[String]("paypal.secret")
+
 
   /**
    * PayPal の API を叩くのに必要な Basic 認証の credential を返す
@@ -34,66 +34,27 @@ class Client @Inject() (
    * @see https://developer.paypal.com/docs/api/orders/v2/#orders_get
    * @param orderId
    */
-  def getOrder(orderId: String): Future[Option[Order]] = {
+  def getOrder(orderId: String) = {
     println(basicCredential())
     ws.url(apiBaseUrl + "/v2/checkout/orders/" + orderId)
       .addHttpHeaders("Authorization" -> ("Basic " + basicCredential()))
       .get()
       .map(response => {
-        println("JSON")
-        println(response.json)
-        print("parse")
-        val result = response.json.asOpt[Order]
-        print("parse")
-        print(result)
-        result
+        println("response.body")
+        println(response.body)
+        println("parsed")
+        println(response.json.validate[Order])
       })
   }
 
-  implicit val orderReads: Reads[Order] = (
-    (__ \ "id").read[String]
-    and (__ \ "status").read[String]
-    // and (__ \ "purchase_units").read[Seq[PurchaseUnit]]
-  )(Order)
-
-  implicit val purchaseUnitReads: Reads[PurchaseUnit] =
-    (__ \ "description").read[String].map(PurchaseUnit)
-
-  implicit val amountRead: Reads[Amount] =
-    (__ \ "currency_code").read[String].map(Amount)
-
+  implicit val jsonReads: Reads[Order] = Json.reads[Order]
 }
 
 /**
  * 注文の詳細
  * @param id 注文ID
  * @param status 注文の状態（注文確定したかどうか）
- * @param purchaseUnits 支払内容の情報
  */
 case class Order(
   id: String,
-  status: String,
-  // purchaseUnits: Seq[PurchaseUnit]
-)
-
-/**
- * 支払いの情報
- * @param amount 金額の情報
- * @param payee 売り手の情報
- * @param payments 支払いに関する情報
- */
-case class PurchaseUnit(
-  // amount: Amount,
-  description: String,
-  // payee: Payee,
-  // payments: Payments
-)
-
-/**
- * 支払い金額についての情報
- * @param currencyCode JPY とか
- * @param breakdown 金額の内訳
- */
-case class Amount(
-  currencyCode: String
 )
