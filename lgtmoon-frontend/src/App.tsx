09326fd84992078ecd7bbbb6c22fb4bb2cb612f8
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2022-09-10 12:07:30 +0900

    基本のレイアウトを作成

--- lgtmoon-frontend/src/App.tsx
+++ lgtmoon-frontend/src/App.tsx
@@ -1,25 +1,114 @@
 import React from 'react';
 import logo from './logo.svg';
+import styled, { createGlobalStyle } from 'styled-components';
 import './App.css';
+
+const AppPage = styled.div`
+`
+
+const AppContent = styled.div`
+`
+
+const Header = styled.header`
+  display: flex;
+  flex-direction: column;
+`
+
+const TitleComponent = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: left;
+`
+
+// ikafont の設定が index.css にある
+const Title = styled.h1`
+  font-family: ikafont;
+  margin: 0;
+  font-size: 4em;
+  
+`
+
+const AppBar = styled.div`
+  background-color: #def;
+  padding: 5px;
+  display: flex;
+  flex-direction: row;
+  gap: 5px;
+`
+
+const TextInput = styled.input`
+  box-sizing: border-box;
+  height: 28px;
+  width: 300px;
+  border-width: 0;
+`
+
+const ButtonBase = styled.button`
+  border: 0;
+  background: #009;
+  color: #fff;
+  font-weight: 700;
+  cursor: pointer;
+`
+
+const SubmitButton = styled(ButtonBase)`
+  height: 28px;
+  width: 100px;
+`
+
+const UploadButton = styled(ButtonBase)`
+  height: 28px;
+  width: 200px;
+`
+
+const TabGroup = styled.div`
+ display: flex;
+ flex-direction: row;
+ justify-content: space-between;
+ width: 100%;
+`
+
+const Tab = styled.div`
+  cursor: pointer;
+  font-weight: 700;
+  font-size: 1.5em;
+  text-align: center;
+  width: 100%;
+  padding: 10px 0;
+
+  &:hover {
+    background-color: #ccf;
+    color: #000;
+  }
+`
+
+const SelectedTab = styled(Tab)`
+  background-color: #009;
+  color: #fff;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppPage>
+      <AppContent>
+        <Header>
+          <TitleComponent>
+            <Title>LGTMoon</Title>
+          </TitleComponent>
+          <AppBar>
+            <TextInput type="text"></TextInput>
+            <SubmitButton>検索</SubmitButton>
+            <UploadButton>画像アップロード</UploadButton>
+          </AppBar>
+          <TabGroup>
+            <SelectedTab>最近の画像</SelectedTab>
+            <Tab>ランダム</Tab>
+            <Tab>お気に入り</Tab>
+            <Tab>使い方</Tab>
+          </TabGroup>
+        </Header>
+      </AppContent>
+    </AppPage>
   );
 }
 
