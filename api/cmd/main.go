07e49b40cf86a7191888b2ffb8b03ca3b464ee60
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2024-02-12 16:41:35 +0900

    update docker

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	port := os.Getenv("PORT")
+	// 環境変数が設定されていない場合は空文字になる
+	if port == "" {
+		port = "8080"
+	}
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "OK",
 		})
 	})
-	r.Run()
+	r.Run("0.0.0.0:" + port)
 }
