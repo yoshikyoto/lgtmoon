62f051325b8b84300ff3c455d5ac9d90853a9e8c
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2024-02-12 18:38:36 +0900

    go db:migrate

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,22 +1,66 @@
 package main
 
 import (
+	"fmt"
+	"lgtmoon-api/internal/model"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-	r := gin.Default()
-	port := os.Getenv("PORT")
-	// 環境変数が設定されていない場合は空文字になる
-	if port == "" {
-		port = "8080"
+	// os.Args[0] にコマンド本体、 os.Args[1] に引数が入るので、長さ2以上必要
+	if (len(os.Args) < 2) {
+		fmt.Println("Command Required e.g. lgtmoon-api serve")
+		fmt.Println("command: serve, migrate, ...")
 	}
-	r.GET("/health", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "OK",
+	command := os.Args[1]
+	fmt.Println("Command: " + command)
+
+	// godotenv.Load のタイミングで err が返ってくる場合があるが、
+	// .env に定義されていなくても環境変数に定義されていれば問題ないので無視して続ける
+	godotenv.Load()
+
+	switch command {
+	case "db:migrate":
+		// https://gorm.io/ja_JP/docs/connecting_to_the_database.html
+		user := os.Getenv("DATABASE_USER")
+		password := os.Getenv("DATABASE_PASSWORD")
+		host := os.Getenv("DATABASE_HOST")
+		port := os.Getenv("DATABASE_PORT")
+		dbName := "lgtmoon"
+
+		// parseTime=true を指定すると時刻が time.Time にパースされる
+		parameter := "charset=utf8mb4&parseTime=True"
+
+		dsn := fmt.Sprintf(
+			"%s:%s@tcp(%s:%s)/%s?%s",
+			user, password, host, port, dbName, parameter,
+		)
+		config := gorm.Config{}
+		db, err := gorm.Open(mysql.Open(dsn), &config)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		db.AutoMigrate(&model.Image{})
+
+	// ライブリロードを行う gin に対応するために default をサーバー起動にしている
+	default:
+		r := gin.Default()
+		port := os.Getenv("PORT")
+		// 環境変数が設定されていない場合は空文字になる
+		if port == "" {
+			port = "8080"
+		}
+		r.GET("/health", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "OK",
+			})
 		})
-	})
-	r.Run("0.0.0.0:" + port)
+		r.Run("0.0.0.0:" + port)
+	}
 }
--- api/internal/model/image.go
+++ api/internal/model/image.go
@@ -2,9 +2,20 @@ package model
 
 import "time"
 
+// gorm の書き方: https://gorm.io/ja_JP/docs/models.html
 type Image struct {
-	id int
-	contentType string
-	createdAt time.Time
-	status int
+	// ID という名前のフィールドは gorm が自動的に主キーとしてくれる
+	ID uint
+	ContentType string
+	// CreatedAt というフィールド名は gorm が自動的に更新してくれる
+	CreatedAt time.Time `gorm:"index"`
+	Status Status
 }
+
+type Status int
+const (
+	// 変換前、変換失敗も全てこのステータスになる
+	NOT_AVAILABLE Status = 0
+	//
+	AVAILABLE Status = 1
+)
